9b94e6a77fab923860b388272b232c3a0d152cde
Steve Malpas <steve@blueygreen.com>
2015-07-25 23:38:37 +0100

    Changed title

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8" />
 	<meta name="robots" content="noindex, nofollow, noarchive" />
-	<title>Test</title>
+	<title>Title for master branch</title>
 	
 	<link rel="stylesheet" href="styles.css" media="screen, projection" />
 	
